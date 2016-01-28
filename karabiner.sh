5285ef98acdb9721b6e1b3f158c504b46571d2b2
Thomas Boyt <thomas@ledgerx.com>
2016-01-28 17:24:42 -0500

    Don't rebind escape for non-Poker 3 inputs

--- karabiner.sh
+++ karabiner.sh
@@ -2,13 +2,14 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-# if I had private keybindings, I could do:
-# private=~/Library/Application\ Support/Karabiner/private.xml
-# cp karabiner_private.xml $private
+private=~/Library/Application\ Support/Karabiner/private.xml
+cp karabiner_private.xml $private
+
+$cli reloadxml
 
 $cli set repeat.wait 33
 /bin/echo -n .
-$cli set remap.escape2backquote 1
+$cli set poker3.escape_backquote 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
